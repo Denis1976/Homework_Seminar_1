9d45a98e6cc697ad95d0cd0069dde8e491fad366
Denis <info.strakhovka@mail.ru>
2022-11-24 00:10:31 +0300

    Homework Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Привет! Введи в строке 1 число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи в строке 2 число, тогда мы узнаем большее их них");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b > a)
+{
+   Console.WriteLine("Мы нашли! Ура! Большее число " +(b));
+}
+else 
+{
+   Console.WriteLine("Мы нашли! Ура! Большее число " +(a));
+} 
