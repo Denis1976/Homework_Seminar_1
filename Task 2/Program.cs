1a8e86e1fff40877147af84514b3982c1cdbcf30
Denis <info.strakhovka@mail.ru>
2022-11-24 23:38:41 +0300

    Задача нахождения наибольшего из 3 чисел

---
+++ Task 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Привет! Введи по порядку 3 разных числа и мы найдем большее среди них");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+} 
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("Наибольшее из введенных чисел -> " + (max));
